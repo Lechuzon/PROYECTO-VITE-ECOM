93806ee7be98279357d81efb9a9c75a4a5253f90
blasct <blas.condori.taipe@gmail.com>
2023-05-02 18:31:43 -0500

    Blog y Contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Home from "../src/pages/Home";
 import OurStore from "./pages/OurStore";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../src/components/Layout";
-
-
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
 
 function App() {
 
@@ -16,6 +16,8 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />}/>
             <Route path="product" element={<OurStore />}/>
+            <Route path="blogs" element={<Blog />}/>
+            <Route path="contact" element={<Contact />}/>
           </Route>
         </Routes>
       </BrowserRouter>
